9a23697dd30db726c5c0626c65de53d3c4f97965
unknown <835127236@qq.com>
2020-05-10 11:43:33 +0800

    236

--- 236.py
+++ 236.py
@@ -16,3 +16,32 @@ class Solution:
         if left:
             return left
         return right
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # containp = set()
+        # containq = set()
+        contain = []
+        def dfs(root):
+            if not root:
+                return False, False
+            a, b = dfs(root.left)
+            c, d = dfs(root.right)
+            # print(root.val, a, b, c, d)
+            if root.val == p.val:
+                if b or d:
+                    contain.append(root)
+                return True, b or d
+            elif root.val == q.val:
+                if a or c:
+                    contain.append(root)
+                return a or c, True
+            else:
+                if (a or c) and (b or d):
+                    contain.append(root)
+                return a or c, b or d
+        dfs(root)
+        # for c in contain:
+        #     print(c.val)
+        return contain[0]
+        # 又是以前写得比现在好
