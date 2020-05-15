e3749a88718aecb5b57dd28a12dfaad21c7c1bdf
unknown <835127236@qq.com>
2020-05-15 11:56:29 +0800

    84

--- 84.py
+++ 84.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def largestRectangleArea(self, heights):
         """
@@ -16,10 +19,53 @@ class Solution:
                 i += 1
             else:
                 height = heights[stack.pop()]
-                print(i,height,stack)
+                print(i, height, stack)
                 if stack:
                     rect = height * (i - stack[-1] - 1)
                 else:
                     rect = height * i
                 res = max(res, rect)
         return res
+
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        # if not heights:
+        #     return 0
+        # ans = 0
+        # for i in range(len(heights)):
+        #     h = heights[i]
+        #     l = 1
+        #     for j in range(i + 1, len(heights)):
+        #         if heights[j] >= h:
+        #             l += 1
+        #         else:
+        #             break
+        #     for j in range(i - 1, -1, -1):
+        #         if heights[j] >= h:
+        #             l += 1
+        #         else:
+        #             break
+        #     ans = max(ans, l * h)
+        # return ans
+        stack = [-1]
+        heights.append(0)
+        ans = 0
+        for i in range(0, len(heights)):
+            while stack and heights[stack[-1]] > heights[i]:
+                right = i - 1
+                height = heights[stack.pop()]
+                left = stack[-1] + 1
+                # print(left, right)
+                ans = max(ans, height * (right - left + 1))
+            stack.append(i)
+            # print(stack)
+        return ans
+
+
+if __name__ == "__main__":
+    s = Solution()
+    hs = [2, 1, 5, 6, 2, 3]
+    hs = [2, 1, 2]
+    print(s.largestRectangleArea(hs))
+    # 又是以前做过的又忘了
