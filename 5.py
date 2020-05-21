3620167db4e0ca193e8792ecc79789222d2ed4f7
unknown <835127236@qq.com>
2020-05-21 11:31:27 +0800

    5

--- 5.py
+++ 5.py
@@ -16,8 +16,30 @@ class Solution:
             expand_str(i, i+1)
         print(self.left, self.max_len)
         return s[self.left: self.left + self.max_len]
-        
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        dp = [[False] * len(s) for _ in range(len(s))]
+        ans = ''
+        for size in range(1, len(s) + 1):
+            for left in range(len(s)):
+                right = left + size - 1
+                if right > len(s) - 1:
+                    break
+                if size == 1:
+                    dp[left][right] = True
+                elif size == 2:
+                    dp[left][right] = s[left] == s[right]
+                else:
+                    dp[left][right] = dp[left+1][right-1] and s[left] == s[right]
+                if dp[left][right] and size > len(ans):
+                    ans = s[left: right + 1]
+        for r in dp:
+            print(r)
+        return ans
+
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.longestPalindrome('cbbd'))        
+    print(s.longestPalindrome('babad'))        
