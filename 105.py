4f94d3872fbb98553721c1f1a7962627648c0dfe
unknown <835127236@qq.com>
2020-05-22 12:14:28 +0800

    105

--- 105.py
+++ 105.py
@@ -17,3 +17,22 @@ class Solution:
         root.left = self.buildTree(preorder[1:1+position], inorder[:position])
         root.right = self.buildTree(preorder[1+position:], inorder[position + 1:])
         return root
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        def build(p1, p2, i1, i2):
+            if p1 == p2:
+                return TreeNode(preorder[p1])
+            if p1 > p2 or i1 > i2:
+                return None
+            idx_of_root = inorder.index(preorder[p1])
+            num_left = idx_of_root - i1
+            num_right = i2 - idx_of_root
+            node = TreeNode(preorder[p1])
+            node.left = build(p1+1, p1+num_left, i1, idx_of_root-1)
+            node.right = build(p2-num_right+1, p2, idx_of_root+1, i2)
+            return node
+        # if not preorder:
+        #     return None
+        return build(0, len(preorder)-1, 0, len(inorder)-1)
