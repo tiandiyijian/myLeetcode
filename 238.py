3141949e3dd1be8545862548588370479f5bfd4c
unknown <835127236@qq.com>
2020-06-04 15:19:05 +0800

    238-2

--- 238.py
+++ 238.py
@@ -12,6 +12,17 @@ class Solution:
         return ans
 
 
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = [1] * len(nums)
+        for i in range(1, len(nums)):
+            ans[i] = ans[i-1] * nums[i-1]
+        post = 1
+        for i in range(len(nums)-1, -1, -1):
+            ans[i] *= post
+            post *= nums[i]
+        return ans
+
 if __name__ == "__main__":
     s = Solution()
     print(s.productExceptSelf([1,2,3,4]))         
