e9ad9eb43d92284956be0e96fb4adc2bf0ce126b
unknown <835127236@qq.com>
2020-06-16 11:34:02 +0800

    297v2

--- 297.py
+++ 297.py
@@ -1,3 +1,6 @@
+import collections
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -23,7 +26,6 @@ class Codec:
             self.serialize(root.right)
         return self.nodes
 
-
     def deserialize(self, data):
         """Decodes your encoded data to tree.
 
@@ -31,6 +33,7 @@ class Codec:
         :rtype: TreeNode
         """
         self.listOfNodes = data.split(',')
+
         def deserialize(listOfNodes):
             val = listOfNodes.pop(0)
             if val == '':
@@ -47,6 +50,59 @@ class Codec:
                 return root
         root = deserialize(self.listOfNodes)
         return root
+
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        ans = ''
+        q = collections.deque([root])
+        while q:
+            node = q.pop()
+            if node:
+                ans += str(node.val) + ','
+                q.appendleft(node.left)
+                q.appendleft(node.right)
+            else:
+                ans += '#,'
+        print(ans)
+        return ans
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        if data[0] == '#':
+            return None
+        val = collections.deque(data.split(','))
+        tmp_nodes = collections.deque([TreeNode(int(val[0]))])
+        root = tmp_nodes[0]
+        val.popleft()
+        while tmp_nodes:
+            length = len(tmp_nodes)
+            for _ in range(length):
+                node = tmp_nodes.pop()
+                tmp = val.popleft()
+                # print(tmp, val)
+                left = None if tmp == '#' else TreeNode(int(tmp))
+                tmp = val.popleft()
+                # print(tmp, val)
+                right = None if tmp == '#' else TreeNode(int(tmp))
+                node.left = left
+                node.right = right
+                if left:
+                    tmp_nodes.appendleft(left)
+                if right:
+                    tmp_nodes.appendleft(right)
+        return root
+
 
 if __name__ == '__main__':
     a = TreeNode(1)
