88ae1741d6053378a475d7cf22d01c9d9a5addc7
unknown <835127236@qq.com>
2020-07-07 10:23:11 +0800

    112v2

--- 112.py
+++ 112.py
@@ -11,3 +11,18 @@ class Solution:
         if root.left is None and root.right is None:
             return root.val == sum
         return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
+
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        def dfs(node, tmp_sum):
+            if not node:
+                return False
+            if not node.left and not node.right:
+                if tmp_sum + node.val == sum:
+                    return True
+                return False
+            return dfs(node.left, tmp_sum + node.val) or dfs(node.right, tmp_sum + node.val)
+        return dfs(root, 0)
