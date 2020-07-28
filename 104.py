fd6cc7fb249169f1d78039c7b428351d56f5d0a4
unknown <835127236@qq.com>
2020-07-28 21:53:15 +0800

    104v2

--- 104.py
+++ 104.py
@@ -15,7 +15,12 @@ class Solution:
         self.ans = 0
         dfs(root, 0)
         return self.ans
+    
+    def maxDepth2(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.maxDepth(TreeNode(0)))
+    print(s.maxDepth2(TreeNode(0)))
