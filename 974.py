fee1d23dfd8102a40605196b682d13184df1a076
unknown <835127236@qq.com>
2020-08-15 22:40:37 +0800

    974v2

--- 974.py
+++ 974.py
@@ -12,8 +12,19 @@ class Solution:
             ans += record[sumArray[i+1]]
             record[sumArray[i+1]] += 1
         return ans
+    
+    def subarraysDivByK2(self, A: List[int], K: int) -> int:
+        remainder_count = collections.defaultdict(int)
+        remainder_count[0] = 1
+        current_remainder = 0
+        ans = 0
+        for i in range(len(A)):
+            current_remainder = (current_remainder + A[i]) % K
+            ans += remainder_count[current_remainder]
+            remainder_count[current_remainder] += 1
+        return ans
 
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.subarraysDivByK([4, 5, 0, -2, -3, 1], 5))
+    print(s.subarraysDivByK2([4, 5, 0, -2, -3, 1], 5))
