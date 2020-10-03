6e364a3950d0ee5d660c268d2ce756dfe96d0034
unknown <835127236@qq.com>
2020-10-03 16:59:11 +0800

    1

--- 1.py
+++ 1.py
@@ -1,8 +1,16 @@
 from typing import List
 
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        history = {nums[0]: 0}
+        for i in range(1, len(nums)):
+            if target - nums[i] in history:
+                return [history[target - nums[i]], i]
+            else:
+                history[nums[i]] = i
 
 
 if __name__ == "__main__":
-   s = Solution()
-   print()
+    s = Solution()
+    print()
