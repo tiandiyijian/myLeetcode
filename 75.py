5c63cdb9d1d69b001ed9efc4189f57aaddc1f484
unknown <835127236@qq.com>
2020-10-07 19:01:34 +0800

    75v2

--- 75.py
+++ 75.py
@@ -23,3 +23,20 @@ class Solution:
         tem = nums[i]
         nums[i] = nums[j]
         nums[j] = tem
+
+    def sortColors2(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l, r = 0, len(nums) - 1
+        i = 0
+        while i <= r:
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+            else:
+                i += 1    
