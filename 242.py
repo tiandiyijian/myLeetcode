0a82ab789eeb8c630a540d57aef134cb79c825f4
unknown <835127236@qq.com>
2020-11-22 20:40:52 +0800

    242

---
+++ 242.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print()
