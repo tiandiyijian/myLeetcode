f54348f152103223c37578aaff4b9485128b0484
unknown <835127236@qq.com>
2020-12-19 19:18:00 +0800

    48

---
+++ 48.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n // 2):
+            matrix[i], matrix[n-1-i] = matrix[n-1-i], matrix[i]
+
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print()
