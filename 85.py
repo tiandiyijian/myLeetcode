6dc13bf24e7193b0e802cdf3971b7b36e60acd00
unknown <835127236@qq.com>
2020-12-26 10:58:24 +0800

    85v2

--- 85.py
+++ 85.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         if not matrix:
@@ -26,3 +29,34 @@ class Solution:
             for j in range(length):
                 maxRec = max(maxRec, (right[j] - left[j]) * height[j])
         return maxRec
+
+    def maximalRectangle2(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+        m, n = len(matrix), len(matrix[0])
+        left = [[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    left[i][j] = 1 if j == 0 else (left[i][j-1] + 1)
+
+        max_rec = 0
+        for j in range(n):
+            stack = []
+            i = 0
+            while i <= m:
+                if not stack or (i < m and left[i][j] >= left[stack[-1]][j]):
+                    stack.append(i)
+                    i += 1
+                else:
+                    height = left[stack.pop()][j]
+                    width = i - 1 - stack[-1] if stack else i
+                    max_rec = max(max_rec, height * width)
+        return max_rec
+
+
+if __name__ == "__main__":
+    s = Solution()
+    m = [["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"],
+         ["1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]]
+    print(s.maximalRectangle2(m))
