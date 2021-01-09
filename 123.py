e5eeb6d9fc21be96ee94ac735ec09ef85b6f9e43
unknown <835127236@qq.com>
2021-01-09 17:52:35 +0800

    123

--- 123.py
+++ 123.py
@@ -18,6 +18,19 @@ class Solution:
         # print(profits)
         # profits.sort()
         # return sum(profits[-2:])
+        if not prices:
+            return 0
+        n = len(prices)
+        buy1 = buy2 = -prices[0]
+        sell1 = sell2 = 0
+
+        for i in range(1, n):
+            buy1 = max(buy1, -prices[i])
+            sell1 = max(sell1, buy1 + prices[i])
+            buy2 = max(buy2, sell1 - prices[i])
+            sell2 = max(sell2, buy2 + prices[i])
+
+        return sell2
 
 
 if __name__ == "__main__":
