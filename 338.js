1f443ae43ecbfbbe2466686f7aa000c52444829a
unknown <835127236@qq.com>
2021-03-03 17:37:47 +0800

    338

---
+++ 338.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=338 lang=javascript
+ *
+ * [338] 比特位计数
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {number[]}
+ */
+var countBits = function (num) {
+  const bits = new Array(num + 1).fill(0);
+  for (let i = 1; i <= num; i++) {
+    bits[i] = bits[i >> 1] + (i & 1);
+  }
+  return bits;
+};
+// @lc code=end
