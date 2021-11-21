05b2488b829eb705f720b22f50bbb071f1b731fc
unknown <835127236@qq.com>
2021-11-21 23:06:30 +0800

    559PY

---
+++ 559.py
@@ -0,0 +1,16 @@
+from typing import List
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        # if root is None:
+        #     return 0
+        return 1 + max((self.maxDepth(child) for child in root.children), default=0) if root else 0
