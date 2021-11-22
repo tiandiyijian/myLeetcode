c2f1060f69d41416f66e738a8387ede6614e490d
unknown <835127236@qq.com>
2021-11-22 18:41:15 +0800

    384GO

---
+++ 384.go
@@ -0,0 +1,34 @@
+package leetcode
+
+import "math/rand"
+
+type Solution struct {
+	nums, original []int
+}
+
+func Constructor(nums []int) Solution {
+	return Solution{
+		nums,
+		append([]int(nil), nums...)}
+}
+
+func (this *Solution) Reset() []int {
+	copy(this.nums, this.original)
+	return this.nums
+}
+
+func (this *Solution) Shuffle() []int {
+	n := len(this.nums)
+	for i := 0; i < n; i++ {
+		j := i + rand.Intn(n-i)
+		this.nums[i], this.nums[j] = this.nums[j], this.nums[i]
+	}
+	return this.nums
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.Reset();
+ * param_2 := obj.Shuffle();
+ */
