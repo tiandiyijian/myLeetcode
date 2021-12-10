4208b2770a428d9f81a41b861936af012819469a
tiandiyijian <835127236@qq.com>
2021-12-10 10:30:26 +0800

    748PY

---
+++ 748.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
+        licensePlate = [c.lower() for c in licensePlate if c.isalpha()]
+        C0 = Counter(licensePlate)
+        ans = None
+        ans_len = float('inf')
+        for w in words:
+            if len(w) < ans_len:
+                if not C0 - Counter(w):
+                    ans = w
+                    ans_len = len(ans)
+        return ans
