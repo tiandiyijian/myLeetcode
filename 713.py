0743ffbfc9425768589ffcfdb8444ca7535b2cce
tiandiyijian <835127236@qq.com>
2022-05-06 11:29:11 +0800

    update 713PY

--- 713.py
+++ 713.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
 
-    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+    def numSubarrayProductLessThanK1(self, nums: List[int], k: int) -> int:
         if k <= 1:
             return 0
 
@@ -25,6 +25,21 @@ class Solution:
 
             print(l, r, cur, ans)
 
+        return ans
+    
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <= 1:
+            return 0
+        l = ans = 0
+        product = 1
+        print(nums, k)
+        for r in range(len(nums)):
+            product *= nums[r]
+            while l <= r and product >= k:
+                product //= nums[l]
+                l += 1
+            ans += r - l + 1
+            print(l, r, ans)
         return ans
 
 
