fdaee5f2617d6828426274ad3b4864b67c7e91d3
tiandiyijian <835127236@qq.com>
2022-07-16 11:04:26 +0800

    offer2/041PY

--- 145.py
+++ 145.py
@@ -1,9 +1,12 @@
+from typing import List
+
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     def postorderTraversal(self, root):
