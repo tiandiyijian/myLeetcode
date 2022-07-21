3e11eb44969167c515c956c8ecf49ccd1633f3be
tiandiyijian <835127236@qq.com>
2022-07-21 11:39:27 +0800

    814PY

--- 814.py
+++ 814.py
@@ -1,3 +1,7 @@
+from functools import cache
+from typing import Optional
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -6,6 +10,7 @@ class TreeNode:
 
 class Solution:
     def pruneTree(self, root: TreeNode) -> TreeNode:
+        @cache
         def legalSubTree(node: TreeNode):
             if node is None:
                 return False
@@ -28,4 +33,18 @@ class Solution:
                 if node.right is not None:
                     nextNodes.append(node.right)
                 nodes = nextNodes
+        return root
+
+
+    def pruneTree2(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # 三年前的代码真是思路清奇啊
+        if not root:
+            return None
+        
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+
+        if not root.left and not root.right and root.val == 0:
+            return None
+        
         return root
