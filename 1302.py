6ceb9c154dd854b6bfc4b819d5e808f64fe05ea7
tiandiyijian <835127236@qq.com>
2022-08-17 09:48:59 +0800

    1302PY

---
+++ 1302.py
@@ -0,0 +1,28 @@
+from turtle import left
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        q = [root]
+        while q:
+            new_q = []
+            for node in q:
+                if node.left:
+                    new_q.append(node.left)
+                if node.right:
+                    new_q.append(node.right)
+            
+            if not new_q:
+                return sum(node.val for node in q)
+
+            q = new_q
+            
