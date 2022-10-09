28381ddb86c2c6bcce49671cdfe62313099c5a88
tiandiyijian <835127236@qq.com>
2022-10-09 23:26:47 +0800

    856PY-2

--- 856.py
+++ 856.py
@@ -1,5 +1,4 @@
 class Solution:
-
     def scoreOfParentheses(self, S: str) -> int:
         # stack = []
         # for i in S:
@@ -29,9 +28,27 @@ class Solution:
                 stack.append(0)
             else:
                 v = stack.pop()
-                stack[-1]+=max(2*v, 1)
+                stack[-1] += max(2 * v, 1)
             print(stack)
         return stack.pop()
+
+    def scoreOfParentheses2(self, s: str) -> int:
+        stk = []
+
+        for c in s:
+            if c == '(':
+                stk.append(-1)
+                cur = 0
+            else:
+                if stk[-1] == -1:
+                    stk[-1] = 1
+                else:
+                    cur = 0
+                    while stk[-1] > 0:
+                        cur += stk.pop()
+                    stk[-1] = cur << 1
+
+        return sum(stk)
 
 
 if __name__ == '__main__':
